8b358825c9b63a0878b7aa7f17e632d89fa572ed
Shahid Ikram <shikram@microsoft.com>
2019-07-06 23:28:55 +0530

    quiz_component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
+import Quiz from "./Quiz";
 
 class App extends Component {
   render() {
     return (
-      <div />
-    )
+      <div>
+        <Quiz />
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
